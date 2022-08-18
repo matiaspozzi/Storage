1d72b9cf0b02731ea561763c2c42511f4b53e056
Matias Pozzi <matias_age01@hotmail.com>
2022-08-18 16:38:06 -0300

    agregue carrito de compras

--- main.js
+++ main.js
@@ -5,34 +5,39 @@ class servicio{
     }
 }
 
+let section = document.getElementById("sectionServices")
 
-const miCarrito = []
+let miCarrito = []
+if(localStorage.getItem("miCarrito")){
+    miCarrito=JSON.parse(localStorage.getItem("miCarrito"));
+// cargo los servicios abandonados a la tabla
+}
 
 
 const services = [
-    {
+    { 
         id: 1,
         servicio: "Diseño y maquetacion de sitio responsivo",
         precio: 70000
     },
     {
         id: 2,
-        servicio: "diseño y maquetacion de sitio HTML5 y CSS3",
+        servicio: "Diseño y maquetacion de sitio HTML5 y CSS3",
         precio: 70000
     },
     {
         id: 3,
-        servicio: "banners animados", 
+        servicio: "Banners animados", 
         precio: 20000
     },
     {   
         id: 4,
-        servicio: "galeria de imagenes",
+        servicio: "Galeria de imagenes",
         precio: 7000
     },
     {
         id: 5,
-        servicio: "formulario web",
+        servicio: "Formulario web",
         precio: 5000
     },
     {    
@@ -48,49 +53,58 @@ const services = [
     
 // inyecto las cards     
 
-let section = document.getElementById("sectionServices")
-for (const service of services){
-    let carta = document.createElement("div")
-    carta.className="card col-md-3";
-    carta.innerHTML=`
-    <div class="card-body">
+renderizarProductos();
+
+function renderizarProductos () {
+    for (const service of services){
+        section.innerHTML+= `<div class="col-md-3" list-group-item"> 
+        <h7> ID: ${service.id}
         <h5 class="card-title">${service.servicio}</h5>
-        <h6 class="card-subtitle mb-2 text-muted">${service.precio}</h6>
-        <button id="btn${service.id}" btn-primary boton">COMPRAR</button>
+        <h6 class="card-subtitle mb-2 text-muted">$ ${service.precio}</h6>
+        <button id="btn${service.id}" btn-primary boton">COMPRAR</button> 
     </div>
     `;
 
-    section.append(carta);
+    
       
    
 
  //Eventos Boton
+ let boton = document.getElementById(`btn${service.id}`) 
+ document.getElementById("boton"), boton.addEventListener("click",() => {
+ console.log ("agregaste al carrito")
+ miCarrito.push (service);
+ })
 
-let boton = document.getElementById(`btn${service.id}`) 
-        document.getElementById("boton"), boton.addEventListener("click",() => {
-        console.log ("Agregaste el producto al carrito")
-        miCarrito.push (service);
-        })
          
 
 }
 
+services.forEach(service =>{
+
+document.getElementById(`btn${service.id}`).addEventListener("click", function() {
+            agregarAlCarrito(service);
+
+ });
+})
+
+}
 
 
+function agregarAlCarrito(service){
+    miCarrito.push(service);
+    console.log(miCarrito);
+    alert("Producto: "+service.servicio+" agregado al carrito")
+    document.getElementById("tableBody").innerHTML+=`
+    <tr>
+        <td>${service.id}</td>
+        <td>${service.servicio}</td>
+        <td>${service.precio}</td>
+    </tr>`
+  ;
+    localStorage.setItem("miCarrito",JSON.stringify(miCarrito));
 
-
-
-
-
-
-
-
-    
-    
-
-       
-
-
+}
 
 
 
